ad59a1606db71f3221aed92bb2bf534a2c73c9b3
Mikhail Feoktistov <mihan0@mail.ru>
2023-08-28 22:53:59 +0300

    encryption password

--- src/main/java/ru/feoktitsov/springcourse/FirstSecurityApp/config/SecurityConfig.java
+++ src/main/java/ru/feoktitsov/springcourse/FirstSecurityApp/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -28,7 +29,8 @@ public class SecurityConfig {
     public AuthenticationManager authManager(HttpSecurity http) throws Exception {
         AuthenticationManagerBuilder authenticationManagerBuilder =
                 http.getSharedObject(AuthenticationManagerBuilder.class);
-        authenticationManagerBuilder.userDetailsService(personDetailsService);
+        authenticationManagerBuilder.userDetailsService(personDetailsService)
+                .passwordEncoder(getPasswordEncoder());
 
         return authenticationManagerBuilder.build();
     }
@@ -46,12 +48,16 @@ public class SecurityConfig {
                         .loginProcessingUrl("/process_login")
                         .defaultSuccessUrl("/hello", true)
                         .failureUrl("/auth/login?error")
-                );
+                )
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/auth/login"));
+
         return http.build();
     }
 
     @Bean
     public PasswordEncoder getPasswordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/ru/feoktitsov/springcourse/FirstSecurityApp/services/RegistrationService.java
+++ src/main/java/ru/feoktitsov/springcourse/FirstSecurityApp/services/RegistrationService.java
@@ -1,9 +1,7 @@
 package ru.feoktitsov.springcourse.FirstSecurityApp.services;
 
-import jakarta.persistence.Transient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.feoktitsov.springcourse.FirstSecurityApp.models.Person;
@@ -12,14 +10,18 @@ import ru.feoktitsov.springcourse.FirstSecurityApp.repositories.PeopleRepository
 @Service
 public class RegistrationService {
     private final PeopleRepository peopleRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public RegistrationService(PeopleRepository peopleRepository) {
+    public RegistrationService(PeopleRepository peopleRepository, PasswordEncoder passwordEncoder) {
         this.peopleRepository = peopleRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Transactional
     public void register(Person person)  {
+        person.setPassword(passwordEncoder.encode(person.getPassword()));
+
         peopleRepository.save(person);
     }
 }
